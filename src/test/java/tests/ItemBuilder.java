4775c65bada56b5330fe4eba499ea54e0924ba2c
Gaius Fonseca <gaiusfonseca@gmail.com>
2022-10-09 01:42:51 -0300

    refatorando ItemBuilder.generateUnitValue para utilizar ItemBuilder.generateValueInRange

--- src/test/java/tests/ItemBuilder.java
+++ src/test/java/tests/ItemBuilder.java
@@ -31,23 +31,23 @@ public class ItemBuilder {
 		switch(index){
 			case 0:
 				//geladeiras terão preço entre 2000 e 3000 com variação de 50
-				unitValue = (generator.nextInt(21) + 40)  * 50;
+				unitValue = generateValueInRange(2000, 3000, 50);
 				break;
 			case 1:
 				//Fogões terão preço entre 1000 e 2000 com variação de 50
-				unitValue = (generator.nextInt(21) + 20) * 50;
+				unitValue = generateValueInRange(1000, 2000, 50);
 				break;
 			case 2:
 				//Microondas terão preço entre 250 e 600 com variação de 25
-				unitValue = (generator.nextInt(15) + 10) * 25;
+				unitValue = generateValueInRange(250, 600, 25);
 				break;
 			case 3:
 				//TVs terão preço entre 1500 e 5000 com variação de 100
-				unitValue = (generator.nextInt(36) + 15) * 100;
+				unitValue = generateValueInRange(1500, 5000, 100);
 				break;
 			default:
 				//Maquinas de lavar terão um valor entre 750 e 2500 com variacao de 75
-				unitValue = (generator.nextInt(8) + 3) * 250;
+				unitValue = generateValueInRange(750, 2500, 75);
 		}
 		
 		return unitValue;
